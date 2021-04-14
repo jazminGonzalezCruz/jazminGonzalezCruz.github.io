a3c2d8cdf0bdb6a7ed37c657eca667a8ba68878a
jazminGonzalezCruz <82421475+jazminGonzalezCruz@users.noreply.github.com>
2021-04-13 20:01:03 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,10 @@
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyBX6iX4rnEy0XKT8aK4NxJcfLwgpPk6S-8",
+  authDomain: "dispiot.firebaseapp.com",
+  projectId: "dispiot",
+  storageBucket: "dispiot.appspot.com",
+  messagingSenderId: "801920513851",
+  appId: "1:801920513851:web:2cf169e26499c70c2de91e"
+
+});
